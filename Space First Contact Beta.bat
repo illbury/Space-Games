696ab9879b916b3da2080db206f78b65af650b97
SaggyDog1 <47309213+SaggyDog1@users.noreply.github.com>
2019-02-03 19:53:25 -0600

    Add files via upload

---
+++ Space First Contact Beta.bat
@@ -0,0 +1,511 @@
+@echo off
+title Space                                                                           Fish co.
+echo 1 for more info on the dev and other games and 2 to begin
+set /p _sa=
+if "%_sa%"=="1" goto Gameinfo
+if "%_sa%"=="2" goto Loadingscreen
+cls
+
+:Gameinfo
+echo This game is made by Jake Daivs
+timeout 2 >nul
+echo The game that you are playing right now was made at his house but his other game was made at school.
+echo Therefore the game made at school is not as developed as this one. The games name is "Space".
+echo (There is a link also to show that this isn't a virus.)
+echo If you want a link to it type "link" other wise type "start".
+set /p _LOLA=
+if "%_LOLA%"=="Link" goto Link
+if "%_LOLA%"=="start" goto Loadingscreen
+cls
+pause
+
+:Link
+echo here is a link that you can use to show that this isn't a virus 
+start "" https://mega.nz/#!zH5RxIRa!_KdugLnM3mbu23xVanPMpbljLB4RfgGtZpZGz0OKrG4
+start "" https://www.robvanderwoude.com/batchcommands.php
+echo waiting 10 seconds so links load and you have time to exit game to evaluate the code. :8)
+timeout 10 >nul
+goto Loadingscreen
+pause
+
+:Loadingscreen
+color 0a
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo                                 =   0 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo []                              =   5 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][]                            =  15 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][]                          =  23 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][]                        =  30 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][]                      =  38 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][]                      =  42 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][]                    =  45 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][]                  =  48 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][]                =  50 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.           Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][]              =  56 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][]            =  63 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][]          =  69 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][]        =  75 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][]      =  79 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][]    =  86 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][]  =  90 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][]  =  96 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        .Complete.
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][][]= 100 ]
+echo ---------------------------------------
+cls
+echo                                                         SPACE 
+timeout 2 >nul    
+echo F
+timeout 2 >nul
+cls
+echo FI
+timeout 2 >nul
+cls
+echo FIR
+timeout 2 >nul
+cls
+echo FRIS
+timeout 2 >nul
+cls
+echo FIRST
+timeout 2 >nul
+cls
+ECHO FIRST C
+timeout 2 >nul
+cls
+echo FIRST CO
+timeout 2 >nul
+cls
+echo FIRST CON
+timeout 2 >nul
+cls
+echo FIRST CONT
+timeout 2 >nul
+cls
+echo FIRST CONTA
+timeout 2 >nul
+cls
+echo FIRST CONTAC
+cls
+timeout 2 >nul
+cls
+echo FIRST CONTACT
+timeout 4 >nul
+cls
+echo                                                            `````````````````-/:-.`                
+timeout 2 >nul
+echo                                                       ```````          `  ```./hyyys+/:`````       
+timeout 2 >nul
+echo              ``````````````                      ```...``            ```  ````.yyyyyyyho.:shyo:    
+timeout 2 >nul
+echo       ````````````````````.-.`               ``````````````      `````````````-hyyhhhhhs.`./hhh-   
+timeout 2 >nul
+echo       -.```````````````..-......`        ``````````````````.`````````````````.shhhhhhhh+````/hd/   
+timeout 2 >nul
+echo       .:.`````````````.--..........` `...````````````````````.``````````````.shhhhhhhhs-````-yy`   
+timeout 2 >nul
+echo ```````.:-``````````````---........-----.```````````````````````````````````:hhhhhhhho-```.:++.    
+timeout 2 >nul
+echo `:-``````-:-``````````````-:-------..``.--`.-````````````......``.````````````:shhdhs:.``.:/+:`     
+timeout 2 >nul
+echo `-:-.````-/-``````````````.:---..```....`````.````````-/osyys+-``.````````````.-/:-.``.:/+/`       
+timeout 2 >nul
+echo   `.:-.```:/:.``````````````````....``````````..`````:ohhyyyhh+```.````````````````.-:++:`         
+timeout 2 >nul
+echo     `.-:-.`:/:.``````````````....```````````````.```.+shhhhhhy:```.`````````````.-:/++-`           
+timeout 2 >nul
+echo        .-:-.:/:.`````````....```````````````````.-.``:+syyyy+-`````.``````````.-/++/.`             
+timeout 2 >nul
+echo          `-:://:-````....````````````.-////-.````..```.-::-.````````.`````.--//+/-`                
+timeout 2 >nul
+echo         `..-.--::-..-.``````````````/oyyyyyy/`````..````````````````.``.-://++:.`                  
+timeout 2 >nul
+echo      `-:::-......-:-.``````````````/shhyyyhd+.````.-...````````````.--://++/.`                     
+timeout 2 >nul
+echo     `ss+:://-......:-.`````````````+shhhhhho-``....````..`````...-::/+++/-`                        
+timeout 2 >nul
+echo   -+yy/-.`.:+/-.....-:.````````````./+ooo+:...```       `....--:///++/-`                           
+timeout 2 >nul
+echo  `ssNMd+-.`.-/s:.....-:.`````````````.......```        ````.:///++:-`                              
+timeout 2 >nul
+echo  .ooNMMd:-..--+y/.....-:.``````````.....````````````````````.:/:.`                                 
+timeout 2 >nul
+echo   ++dNMMo::/++oyh/.....--```````.-..`````````````````````...````.                                  
+timeout 2 >nul
+echo   .oshmN++/:+hmmhh:.-..-:.``````::.``````````````````.....``````.`                                 
+timeout 2 >nul
+echo -+ysooo/--..-+s+::y:-:-::-......:/:-.`````````````...-``````````.                                  
+timeout 2 >nul
+echo `soNMm+-.`:o+:-.``.s+::::::.-------://-.````````.--.`````````````.                                  
+timeout 2 >nul
+echo `soNMMm:-oomNd+-.``:y::::::-------:/+o+/:.``.....````````````````.                                  
+timeout 2 >nul
+echo ++dNMMo:y+mMMm/....h://:::-::/+ooosooo/-::--.```````````````````.                                  
+timeout 2 >nul
+echo `+shmN++o/hNMMy-:::d/////+sssssso/:-`   `-/:.``````````````````.`                                  
+timeout 2 >nul
+echo   -/+:.``+shNNy++oyy////:+o+:-.`          `-::-````````````````.`                                  
+timeout 2 >nul
+echo          `:oss:+ss+--..```                  `.::-``````````````-                                   
+timeout 2 >nul
+echo                                               `.::-.`````````.--                                   
+timeout 2 >nul
+echo                                                  .::-.`````.:-.                                    
+timeout 2 >nul
+echo                                                    `-:-..--.`                                      
+timeout 2 >nul
+echo                                                      `...`        
+timeout 5 >nul
+cls
+echo Do you wish to start? Yes - 1 No - 2
+set /p _SartP=
+if "%_StartP%"=="1" goto Start
+if "%_StartP%"=="2" exit
+pause
+
+:Start
+cls
+echo ____________________________________________________________________________________________
+echo                                                                                               
+title                                                 Space
+echo                                            Welcome to Space
+echo ____________________________________________________________________________________________
+echo                                                                                                          Q to quit
+cls
+pause
+cls
+:namessetup
+echo Name your ship
+set /p _sn=
+echo Ship name is %_sn%
+echo What's your name
+set /p _pn=
+echo Welcome commander %_pn%
+echo Whats your brig officar's name again?
+set /p _bon=
+echo Commanding Officar brig is %_bon%.
+echo Whats the AI's name?
+set /p _AIName=
+echo are these the names you want? Yes - 1 or No - 2
+set /p _names=
+if "%_names%"=="1" goto Start2
+if "%_names%"=="2" goto namessetup
+cls
+pause
+:Start2
+Echo do you wish to start yes - 1 or no - 2?
+set /p _a1=
+if "%_a1%"=="1" goto Answer1
+if "%_a1%"=="2" goto Answer2
+if "%_a1%"=="3" goto exit
+cls
+pause
+
+
+:Answer1
+cls
+title Commanding Brige
+echo Hello Commanding  Officer - %_pn%
+echo hourly report on the ship?- %_pn%
+echo Small amount of engine damage, repair teams on it already.
+echo Hull damage is also small.
+echo The amount of asteriod feilds we went through so far is 90 and counting. - %_bon%
+echo How would you like to reply 1 - good work any word from base ground or 2 - Good work but im replacing you or more like commanding base is replacing you?
+set /p _pa2=
+if "%_pa2%"=="1" goto exploring
+if "%_pa2%"=="2" goto papers
+cls
+pause
+
+:exploring
+echo what soloar system would you like to go to? %_bon%
+echo 1 Milky way
+echo or 2 Retuff
+set /p _ss=
+if "%_ss%"=="1" goto Milkyway
+if "%_ss%"=="2" goto Retuff
+cls
+pause
+
+:Milkyway
+echo Commander we have arived at the Milkyway. - %_bom%
+echo PLEASE STATE YOUR PRESENCE AND REASON FOR BEING HERE!
+echo how do you respond? We are here for a resupply and our ship name is %_sn% - 1. or Say nothing - 2
+set /p _eosorSWMY=
+if "%_eosorSWMY%"=="1" goto EncounterOtherSpeciesMY
+if "%_eosorSWMY%"=="2" goto StartWarMY
+echo You see all space stations 
+cls
+pause
+
+:papers
+title Going to the papers on his firing...
+echo Im sorry but its true...
+echo you have been stabbd...
+echo Wanna go back? 1 - Yes or 2 - No
+set /p _PR=
+if "%_PR%"=="1" goto Milkyway
+if "%_PR%"=="2" goto Answer1
+cls
+pause
+
+:StartWarMY
+title War
+Echo You must respond now or else your ship will be destroyed for not responding for you to enter the milyway
+Echo Sir we have encoming friendly ships that are in battle mode. What should we do? - %_bon%
+echo %_AINme% warn crew members of the imcoming battle ready ship's
+echo Also %_AINme% prepare for hyperspace jump to a random part in the galaxy. - %_pn%
+echo Blaring Sirens start to sound!
+echo loud banging and crashing as %_sn% is hit by two large missles
+echo loud screeching noises as %_sn% takes off in hyper space
+cls
+pause
+
+:EncounterOtherSpeciesMY
+echo Entry Granted welcome %_pn%
+echo Ok to Venus we go %_AIName%. - %_pn%
+echo Ok Sir. %_AIName%
+echo Loud banging and crashing and booming noises
+pause
